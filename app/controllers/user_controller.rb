a418d09cbeda06f0c7da9d1e143c714f7322c44f
jeremybaxter <jrbaxter94@gmail.com>
2024-06-17 14:18:12 -0500

    can add new users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,7 +8,15 @@ class UserController < ApplicationController
     render({ :template => "/user_templates/details"})
   end
 
-  # def updates
+  def create
+    new_user = User.new
+    new_user.username = params.fetch("new_user")
+
+    new_user.save
+    redirect_to("/users/#{new_user.id}")
+  end
+
+  # def update
 
   # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "user", :action => "index" })
   get("/users", { :controller => "user", :action => "index" })
   get("/users/:username", { :controller => "user", :action => "show" })
+  post("/add_user", { :controller => "user", :action => "create" })
   # post("update_user/:username", { :controller => "user", :action => "update" })
 
   get("/photos", { :controller => "photo", :action => "index" })
